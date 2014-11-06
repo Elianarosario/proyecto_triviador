7449b25a6cce7682b54b564d1739a75781c71c94
eliana <rio_14_ni@hotmail.com>
2014-11-05 23:21:13 -0400

    cuarta revision

--- proyect/apps/usuario/models.py
+++ proyect/apps/usuario/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from thumbs import ImageWithThumbsField
+from django.contrib.auth.models import User
 # Create your models here.
+class Perfil(models.Model):
+	user=models.OneToOneField(User,unique=True)
+	avatar=ImageWithThumbsField(upload_to="img_user", sizes=((50,50),(80,80)))
